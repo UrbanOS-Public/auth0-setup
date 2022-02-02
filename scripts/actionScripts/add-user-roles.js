8f272e0eaf5dbb7dbecacda3c2c0db37fffd05f0
Christine Walsh <christine.walsh@accenture.com>
2022-02-02 09:34:35 -0500

    updates based on PR comments
    Datastillery/internal#113

--- scripts/actionScripts/add-user-roles.js
+++ scripts/actionScripts/add-user-roles.js
@@ -1,5 +1,5 @@
 exports.onExecutePostLogin = async (event, api) => {
-    const namespace = 'https://andi.smartcolumbusos.com';
+    const namespace = 'https://andi.urbanos-demo.com';
     if (event.authorization) {
       api.idToken.setCustomClaim(`${namespace}/roles`, event.authorization.roles);
       api.accessToken.setCustomClaim(`${namespace}/roles`, event.authorization.roles);
